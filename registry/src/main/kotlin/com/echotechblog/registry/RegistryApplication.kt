4c8c31f5c14ed947b81812c82b9c4d3224cd09f1
nikolaikriuchkov <ngkrychkov@gmail.com>
2025-01-19 12:25:12 +0000

    WIP registry and embedded hazelcast registration

--- registry/src/main/kotlin/com/echotechblog/registry/RegistryApplication.kt
+++ registry/src/main/kotlin/com/echotechblog/registry/RegistryApplication.kt
@@ -2,8 +2,10 @@ package com.echotechblog.registry
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
+@EnableEurekaServer
 class RegistryApplication
 
 fun main(args: Array<String>) {
