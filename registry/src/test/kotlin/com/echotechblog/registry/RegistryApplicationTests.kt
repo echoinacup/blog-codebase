5d9550f0787603823948cceb5ffdcf3f9e7a8a8a
nikolaikriuchkov <ngkrychkov@gmail.com>
2025-01-19 12:09:47 +0000

    initial commit for registry

---
+++ registry/src/test/kotlin/com/echotechblog/registry/RegistryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.echotechblog.registry
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RegistryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
